0b6c2c4013dd4ee786dc7ba12556bf2d957e4db3
dubing.chen <dobbin.chen@gmail.com>
2025-10-11 23:26:24 +0800

    Initial commit

--- hug_load.py
+++ hug_load.py
@@ -4,7 +4,22 @@ api = HfApi()
 # Upload all the content from the local folder to your remote Space.
 # By default, files are uploaded at the root of the repo
 api.upload_folder(
-    folder_path="/home/yc37439/code/alocc2/work_dir2/alocc/",
+    folder_path="/home/yc37439/code/alocc2/work_dir2/gdfusion/",
+    repo_id="Dobbin/OccStudio",
+    repo_type="model",
+)
+api.upload_folder(
+    folder_path="/home/yc37439/code/alocc2/work_dir2/causalocc/",
+    repo_id="Dobbin/OccStudio",
+    repo_type="model",
+)
+api.upload_folder(
+    folder_path="/home/yc37439/code/alocc2/work_dir2/fbocc/",
+    repo_id="Dobbin/OccStudio",
+    repo_type="model",
+)
+api.upload_folder(
+    folder_path="/home/yc37439/code/alocc2/work_dir2/bevdetocc/",
     repo_id="Dobbin/OccStudio",
     repo_type="model",
 )
